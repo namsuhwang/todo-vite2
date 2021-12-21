0389e38f658fa90bd7afdc4f175218da8401c808
Hwang Namsu <namsuhwang@naver.com>
2021-12-22 07:54:42 +0900

    first commit

---
+++ src/components/TodoListContainer.vue
@@ -0,0 +1,23 @@
+<templete></templete>
+
+<script>
+import { ref, readonly, provide } from 'vue'
+
+export default {
+    name: 'TodoListContainer',
+    setup() {
+        const todos = ref([])
+        provide('todos', readonly(todos))
+        const initTodos = (init_todos) => {
+            todos.Value = init_todos
+        }
+        const addTodo = (dodo, date) => {}
+        const removeTodo = (id) => {}
+        const completeTodo = (id) => {}
+
+        provide('addTodo', addTodo)
+        provide('removeTodo', removeTodo)
+        provide('completeTodo', completeTodo)
+    },
+}
+</script>
